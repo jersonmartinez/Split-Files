9e0cf10e4d72c76f12bec7637c200c6e61dba671
Jerson Martinez <sidemasterdelfuturo@gmail.com>
2018-02-10 12:27:47 -0600

    Adding router and file called 'complete_document.pdf' to example.

--- link.php
+++ link.php
@@ -2,6 +2,6 @@
 	$PathFile = file_get_contents($_SERVER['DOCUMENT_ROOT']."/splitfile/Document/Document1.pdf");
 	$PathFile .= file_get_contents($_SERVER['DOCUMENT_ROOT']."/splitfile/Document/Document2.pdf");
 
-	file_put_contents($_SERVER['DOCUMENT_ROOT']."/splitfile/document_completo.pdf", $PathFile);
+	file_put_contents($_SERVER['DOCUMENT_ROOT']."/splitfile/complete_document.pdf", $PathFile);
 
 ?>
